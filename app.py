58469b095ad0e982def70d6bcc7b78fca7422ff8
amarss321 <96772269+amarss321@users.noreply.github.com>
2023-12-30 12:30:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import pymysql
 app = Flask(__name__)
 
 def get_db_connection():
-    connection = pymysql.connect(host='mydb.cylck8yh5jkc.eu-central-1.rds.amazonaws.com',  # Replace with your RDS endpoint
-                                 user='dbuser',      # Replace with your RDS username
-                                 password='dbpassword',  # Replace with your RDS password
-                                 db='devprojdb',   # Replace with your database name
+    connection = pymysql.connect(host='mydb.cocnsiidqxje.us-east-1.rds.amazonaws.com',  # Replace with your RDS endpoint
+                                 user='amar',      # Replace with your RDS username
+                                 password='Ssamar143s',  # Replace with your RDS password
+                                 db='records',   # Replace with your database name
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
     return connection
